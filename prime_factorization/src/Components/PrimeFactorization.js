1f718a408b39ba41bb0867756dc2b2c0ec78b7c5
yyyavuz <yusufyyavuz@outlook.com>
2021-01-31 18:32:59 +0300

    Closest Prime Number

--- prime_factorization/src/Components/PrimeFactorization.js
+++ prime_factorization/src/Components/PrimeFactorization.js
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const PrimeFactorization = () => {
+export default function PrimeFactorization() {
+  const [value, setValue] = useState();
+  
+    const printPrime = e => {
+    console.log(e.target.value)
+    var primes = [];
+        for(var i = 2; i < e.target.value; i++) {
+            primes[i] = true;
+        }
+    var limit = Math.sqrt(e.target.value);
+        for(var i = 2; i < limit; i++) {
+            if(primes[i] === true) {
+                for(var j = i * i; j < e.target.value; j += i) {
+                    primes[j] = false;
+                }
+            }
+        }
+        for(var i = 2; i < e.target.value; i++) {
+            if(primes[i] === true) {
+                setValue (i)
+                console.log(i + " " + primes[i]);
+            }
+        }
+    }
+
+    useEffect(() => {
+    document.title = value;
+    console.log(value)
+    }, [value]);
 
   return (
-    <div>
-      Hello World
+    <div className="PrimeFactorization">
+      <h3>PrimeFactorization</h3>
+      <input  placeholder="Enter number" onChange={printPrime}/>
+      <p>Closest Prime Number: {value}</p>
     </div>
-    );
-  }
-
-export default React.memo(PrimeFactorization);
+  );
+}
