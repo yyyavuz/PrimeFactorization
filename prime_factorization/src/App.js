57615b0dbce21d110de73a26d4951cf390802fd2
yyyavuz <yusufyyavuz@outlook.com>
2021-01-31 16:46:51 +0300

    Create App

---
+++ prime_factorization/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PrimeFactorization from './Components/PrimeFactorization';
+
+function App() {
+  return (
+    <div className="App">
+      <PrimeFactorization/>
+    </div>
+  );
+}
+
+export default App;
---
+++ prime_factorization/src/Components/PrimeFactorization.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PrimeFactorization = () => {
+
+  return (
+    <div>
+      Hello World
+    </div>
+    );
+  }
+
+export default React.memo(PrimeFactorization);
